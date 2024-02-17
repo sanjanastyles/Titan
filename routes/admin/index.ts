cb84c0e079469c47f85068ebf3468fb877153e26
kuuhaku-u <ryusama6996@gmail.com>
2024-02-17 11:13:08 +0530

    fic(admin): admin flow

--- routes/admin/index.ts
+++ routes/admin/index.ts
@@ -2,7 +2,7 @@
  * @todo - all user API
  */
 import express, { Request, Response, Router } from 'express';
-import { SERVICEMAN_SIGNUP_MODEL } from '../../model';
+import { SERVICEMAN_SIGNUP_MODEL, SERVICE_MODEL } from '../../model';
 // import { createToken } from "../../utils/utils";
 // import { auth } from "../../middleware";
 class AdminController {
@@ -13,35 +13,36 @@ class AdminController {
   }
   private initializeRoutes(): void {
     this.router.get('/users', this.handleAllUsers);
-    // this.router.post("/signup", this.handleSignup);
+    this.router.post('/addservice', this.handleAddService);
+    this.router.post('/block', this.handleBlock);
     // this.router.get("/profile", auth, this.handleProfile);
     // this.router.post("/profile", auth, this.handleProfilePost);
     // this.router.get("/see/reviews", this.handleSeeReviews);
   }
+  private handleBlock = async (req: Request, res: Response): Promise<void> => {
+    try {
+      // const userData = await SERVICEMAN_SIGNUP_MODEL.find();
+      // res.status(200).json({ code: 200, msg: 'Success : User Blocked', data: userData });
+    } catch (err) {
+      res.status(500).json({ msg: 'Internal Server Error', code: 500, error: err.message });
+    }
+  };
   private handleAllUsers = async (req: Request, res: Response): Promise<void> => {
     try {
-      const pipeline: any[] = [
-        {
-          $lookup: {
-            from: 'servicemen',
-            as: 'user',
-          },
-        },
-        { $unwind: '$user' },
-        // { $addFields: { 'user.id': '$_id' } },
-        // {
-        //   $project: {
-        //     _id: 0,
-        //     id: '$user.id',
-        //     name: '$user.name',
-        //     review: '$$ROOT',
-        //   },
-        // },
-      ];
-      const a = await SERVICEMAN_SIGNUP_MODEL.find();
-    //   const b = await
-    //   const users = await SERVICEMAN_SIGNUP_MODEL.aggregate(pipeline);
-      res.status(200).json({ code: 200, msg: 'Success', data: a });
+      const userData = await SERVICEMAN_SIGNUP_MODEL.find();
+      res.status(200).json({ code: 200, msg: 'Success', data: userData });
+    } catch (err) {
+      res.status(500).json({ msg: 'Internal Server Error', code: 500, error: err.message });
+    }
+  };
+  private handleAddService = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { serviceName } = req.body;
+      const newService = new SERVICE_MODEL({
+        serviceName,
+      });
+      await newService.save();
+      res.status(200).json({ code: 200, msg: 'Success' });
     } catch (err) {
       res.status(500).json({ msg: 'Internal Server Error', code: 500, error: err.message });
     }
--- routes/customers/index.ts
+++ routes/customers/index.ts
@@ -1,24 +1,16 @@
 import express, { Request, Response, Router } from "express";
-import { createToken } from "../../utils/utils";
-import { auth } from "../../middleware";
 import {
   REVIEW_MODEL,
-  SERVICEMAN_SIGNUP_MODEL,
-  SERVICE_MODEL,
 } from "../../model";
-
 class CustomerController {
   private router: Router;
-
   constructor() {
     this.router = express.Router();
     this.initializeRoutes();
   }
-
   private initializeRoutes(): void {
     this.router.post("/review", this.handleReviewPost);
   }
-
   private handleReviewPost = async (req: Request, res: Response) => {
     try {
       const {
@@ -29,7 +21,6 @@ class CustomerController {
         associatedServiceman,
         associatedJob,
       } = req.body;
-
       const newReview = new REVIEW_MODEL({
         quality,
         feedback,
@@ -39,7 +30,6 @@ class CustomerController {
         associatedJob,
       });
       await newReview.save();
-
       res.status(200).json({ code: 201, msg: "Created Successfully" });
     } catch (err) {
       res
@@ -47,10 +37,8 @@ class CustomerController {
         .json({ msg: "Internal Server Error", code: 500, error: err.message });
     }
   };
-
   public getRouter(): Router {
     return this.router;
   }
 }
-
 export default CustomerController;
