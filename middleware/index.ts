9337fc5000864613964cda8f73854fcf4de07a48
kor0 <ryusama6996@gmail.com>
2024-02-18 18:08:15 +0530

    6 workflow to do the linting (#8)
    * fix(workflow): add lint workflow

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,18 +1,19 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
-export function  auth(req, res, next) {
-    const authHeader = req.headers.authorization;
-    const accessTokenSecret= process.env['JWT_SECRET'] || '';
-    if (authHeader) {
-      const token = authHeader.split(' ')[1];
-      jwt.verify(token, accessTokenSecret, (err, user) => {
-        if (err) {
-          return res.send({code:403, msg:"Error Processing Token"});
-        }
-        req.body.username = user;
-        next();
-      });
-    } else {
-      res.send({code:401, msg:"Forbidden"})    }
+export function auth(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const accessTokenSecret = process.env['JWT_SECRET'] || '';
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+      if (err) {
+        return res.send({ code: 403, msg: 'Error Processing Token' });
+      }
+      req.body.username = user;
+      next();
+    });
+  } else {
+    res.send({ code: 401, msg: 'Forbidden' });
   }
+}
--- model/index.ts
+++ model/index.ts
@@ -1,6 +1,6 @@
-export { default as CONTACT_MODEL } from "./Contact.ts";
-export { default as SERVICE_MODEL } from "./service";
-export { default as SERVICEMAN_SIGNUP_MODEL } from "./serviceman";
-export { default as REVIEW_MODEL } from "./Review";
-export { default as HISTROY_MODEL } from "./History";
-export { default as BLOCK_MODEL } from "./Block";
+export { default as CONTACT_MODEL } from './Contact.ts';
+export { default as SERVICE_MODEL } from './service';
+export { default as SERVICEMAN_SIGNUP_MODEL } from './serviceman';
+export { default as REVIEW_MODEL } from './Review';
+export { default as HISTROY_MODEL } from './History';
+export { default as BLOCK_MODEL } from './Block';
