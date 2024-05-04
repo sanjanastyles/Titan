dd9fe0fa09c7498b715a709f8541a8b9d8d64404
kor0 <ryusama6996@gmail.com>
2024-05-04 11:10:21 +0530

    feat(enya): AI  model

---
+++ enya/enya-model/install_nltk_deps.py
@@ -0,0 +1,6 @@
+import nltk
+import os
+
+nltk.data.path.append("C:\\Python312\\nltk_data")  
+nltk.download('punkt')
+nltk.download('wordnet')
---
+++ enya/enya-server/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
+
+app = Flask(__name__)
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('C:\\Users\\bonam\\pyy\\model\\intents.json').read())
+words = pickle.load(open('C:\\Users\\bonam\\pyy\\model\\words.pkl', 'rb'))
+classes = pickle.load(open('C:\\Users\\bonam\\pyy\\model\\classes.pkl', 'rb'))
+model = load_model('C:\\Users\\bonam\\pyy\\model\\chatbot_model.h5')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = [{'intent': classes[r[0]], 'probability': str(r[1])} for r in results]
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    try:
+        data = request.json
+        message = data['message']
+        ints = predict_class(message)
+
+        res = get_response(ints, intents)
+        return jsonify({'response': res})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081)
